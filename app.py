22346d6bb521863ff50cc5e8c7be734956c88b91
Shantanu Patil <shantanupatil1899@gmail.com>
2021-05-22 07:53:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    file = request.files['filename']
+    # Read the image via file.stream
+    image = Image.open(file.stream)
+    
+    text_from_pickle = pickle.loads(model)
+    
+    # Use the loaded pickled model to make predictions
+    output = text_from_pickle(image)
+
+    return render_template('index.html', prediction_text='Number Plate: $ {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
