78aa295c8a6dd18cae445d099918081bb4b2ce54
Shantanu Patil <shantanupatil1899@gmail.com>
2021-05-22 08:02:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import cv2
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
@@ -16,14 +17,14 @@ def predict():
     '''
     file = request.files['filename']
     # Read the image via file.stream
-    image = Image.open(file.stream)
+    image = cv2.imread(file)
     
     text_from_pickle = pickle.loads(model)
     
     # Use the loaded pickled model to make predictions
     output = text_from_pickle(image)
 
-    return render_template('index.html', prediction_text='Number Plate: $ {}'.format(output))
+    return render_template('index.html', prediction_text='Number Plate: ' output)
 
 
 if __name__ == "__main__":
